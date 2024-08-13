108f596b748d1e2860ac933d90caec42712d4a7f
Your Name <you@example.com>
2024-08-13 16:28:16 +0700

    first commit

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,10 +1,23 @@
 'use strict';
-
+let total = 0;
 /***
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  // Return a function that adds its argument to the total
+  return function (arg) {
+    if (typeof arg === 'undefined') {
+      // If called without arguments, return the current total
+      // const temp = obj.total;
+      const temp = total;
+      total = 0;
+      return temp;
+    }
+    // If called with a number, add it to the total
+    total += arg;
+    // Return a new function to allow for chaining
+    return makeInfinityAdder(total);
+  };
 }
 
 module.exports = makeInfinityAdder;
